d571e2d4b0a03f8ad154af29a09333a44737b501
Caio Cesar Guimarães Costa <caio.costa@conectaservicos.com.br>
2020-07-19 00:19:11 -0300

    modificando bd para postgre

--- prontuarioEletronico/settings.py
+++ prontuarioEletronico/settings.py
@@ -82,19 +82,23 @@ WSGI_APPLICATION = 'prontuarioEletronico.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'prontuarioEletronico',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prontuario_eletronico',
+        'USER': 'postgres',
+        'PASSWORD': 'dbadmin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'djongo',
+#         'NAME': 'prontuarioEletronico',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
