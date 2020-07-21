51d6ef56a3c33473cbadc38ab723d8fdf1a697f1
Caio Cesar Guimarães Costa <caio.costa@conectaservicos.com.br>
2020-07-21 17:17:34 -0300

    login page responsiva

--- prontuarioEletronico/settings.py
+++ prontuarioEletronico/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p-u)o(cmyxjx9%y_cwyz2sta_%=e0*euxj3pli05fj)ry5%k9y'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.environ.get('DEBUG') == 'True'
+# DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
