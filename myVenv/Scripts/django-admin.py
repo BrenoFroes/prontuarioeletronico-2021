8f3ee29131b8f54f7b9db1bdd4b26e392fd1c091
Breno Froes <breno_froes@id.uff.br>
2021-07-27 10:42:18 -0300

    [adjust] Ajuste vens

--- myVenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\users\breno\documents\projetos\tcc\prontuário eletrônico - caio\prontuarioeletronico\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
